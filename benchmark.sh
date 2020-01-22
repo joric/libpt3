36084384eb39a323e4220ed32166ddc6ea5dde50
joric <joric@users.noreply.github.com>
2020-01-23 00:12:09 +0500

    bashate E010: do not on the same line as for

--- benchmark.sh
+++ benchmark.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-for f in shiru zxssk
-do
+for f in shiru zxssk; do
 a=pt3_reader
 gcc -O3 $a\_$f.c -o $a\_$f
 ./$a\_$f > data\_$f.h
@@ -9,8 +8,7 @@ done
 
 diff -u data_shiru.h data_zxssk.h
 
-for f in ayemu shiru
-do
+for f in ayemu shiru; do
 b=ay_render
 gcc -O3 $b\_$f.c -o $b\_$f
 ./$b\_$f
