14a94d5c9bc5e802ae24991fc7dc65e40924d2b2
joric <joric@users.noreply.github.com>
2020-01-24 02:40:09 +0500

    updated files

--- benchmark.sh
+++ benchmark.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
 for f in shiru zxssk; do
-a=pt3_reader
-gcc -O3 $a\_$f.c -o $a\_$f
-./$a\_$f > data\_$f.h
+gcc -O3 pt3_reader_$f.c -o pt3_reader_$f
+./pt3_reader_$f > data_$f.h
 done
 
 diff -u data_shiru.h data_zxssk.h
 
 for f in ayemu shiru; do
-b=ay_render
-gcc -O3 $b\_$f.c -o $b\_$f
-./$b\_$f
+gcc -O3 ay_render_$f.c -o ay_render_$f
+./ay_render_$f
 done
 
